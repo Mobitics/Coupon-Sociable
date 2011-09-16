d6d6852085d081619b6166bbee58e08ea183cf15
Justin Burdett <justin.burdett@gmail.com>
2011-09-16 15:05:33 -0400

    Adding blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CouponSociable::Application.config.secret_token = 'e683263d2ed12b2fda62264169c0595810678792abe991946ab7bd033d0d8536a89e92cb34aa326d868f9686f28b9bde6da96570555d44bdaf07ffe44e41602b'
