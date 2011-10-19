d0c4a91f8bc8248df8bffb6af37127195a693679
Justin Burdett <justin.burdett@gmail.com>
2011-10-19 14:37:15 -0400

    Fixing Facebook?

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -36,7 +36,7 @@ class CouponsController < ApplicationController
   			if Rails.env == "development"
 				 client = OAuth2::Client.new('224049387650906', 'e8e33bd125343a6107f7220f046b7203', :site => 'https://graph.facebook.com')
 			else
-				client = OAuth2::Client.new('157999927621589', 'b72e2c73b99ff5959c212461fd21b510', :site => 'https://graph.facebook.com'}) # :ssl => {:ca_path => '/etc/ssl/certs
+				client = OAuth2::Client.new('157999927621589', 'b72e2c73b99ff5959c212461fd21b510', :site => 'https://graph.facebook.com') # :ssl => {:ca_path => '/etc/ssl/certs }
 			end
 			 
  			token = OAuth2::AccessToken.new(client, current_user.token)
