79533b18081bb7a2168e7dfa69e5a2937197a569
Justin Burdett <justin.burdett@gmail.com>
2011-10-19 13:32:12 -0400

    More updates for Heorku compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CouponSociable::Application.routes.draw do
-  root :to                   => 'home#index'
+  root :to                   => 'login#index'
 
   match 'welcome'            => 'home#welcome'
 
